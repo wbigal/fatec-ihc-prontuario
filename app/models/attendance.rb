71345492be5f31c4bcb7f9972b38c3ceba063856
Wellison <wellison28@gmail.com>
2017-12-02 09:31:05 -0200

    create tests model Attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,4 +2,6 @@ class Attendance < ApplicationRecord
   belongs_to :person
   belongs_to :doctor
   has_one :permission
+
+  validates_presence_of :date, :symptoms, :diagnostic
 end
--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:date) }
+  it { is_expected.to validate_presence_of(:symptoms) }
+  it { is_expected.to validate_presence_of(:diagnostic) }
+
+  it { is_expected.to have_one(:permission)}
 end
