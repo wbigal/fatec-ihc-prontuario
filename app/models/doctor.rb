fae2d6f1fde0b09b7c3cb96ea8e6f7a8f7dd776b
Wellison <wellison28@gmail.com>
2017-12-02 09:18:24 -0200

    create tests Doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,6 @@ class Doctor < ApplicationRecord
   belongs_to :person
   has_many :attendances
   has_many :permissions
+
+  validates_presence_of :crm
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:crm) }
+
+  it { is_expected.to have_many(:permissions)}
+  it { is_expected.to have_many(:attendances)}
 end
