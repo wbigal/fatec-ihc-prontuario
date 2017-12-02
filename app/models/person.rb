4c5071945e3466c1f1e4f307133ce41d45579001
Wellison <wellison28@gmail.com>
2017-12-02 09:12:37 -0200

    create tests Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ApplicationRecord
 	has_many :permissions
 	has_many :attendances
+
+	validates_presence_of :cns, :email, :password, :birth_date
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:cns) }
+  it { is_expected.to validate_presence_of(:birth_date) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
+
+  it { is_expected.to have_many(:permissions)}
+  it { is_expected.to have_many(:attendances)}
 end
