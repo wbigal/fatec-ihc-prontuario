46693a2c10a8741e457a0a98a31ed65cfe3e5b49
Wagner Bigal <wagnerbigal@gmail.com>
2017-12-03 18:29:11 -0200

    Adding flash message

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -4,6 +4,9 @@ class AuthenticatedController < ApplicationController
   protected
 
   def authenticate_pessoa!
-    redirect_to new_accounts_sessions_path unless pessoa_signed_in?
+    unless pessoa_signed_in?
+      flash[:alert] = 'Você precisa se logar para acessar a página solicitada'
+      redirect_to new_accounts_sessions_path
+    end
   end
 end
