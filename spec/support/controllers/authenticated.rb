4e584a8ba65e1174677c16341214acfd8c36db71
Wagner Bigal <wagnerbigal@gmail.com>
2017-12-03 23:37:06 -0200

    Refactoring

--- spec/support/controllers/authenticated.rb
+++ spec/support/controllers/authenticated.rb
@@ -1,6 +1,17 @@
 RSpec.configure do |config|
   config.before(:each, :authenticated) do
-    pessoa = create(:pessoa, :with_account)
-    controller.current_pessoa = pessoa
+    controller.current_pessoa = current_pessoa
+  end
+
+  config.before(:each, :authenticated_doctor) do
+    controller.current_pessoa = current_pessoa_medico.pessoa
+  end
+
+  def current_pessoa
+    @current_pessoa ||= create(:pessoa, :with_account)
+  end
+
+  def current_pessoa_medico
+    @current_pessoa_medico ||= create(:medico)
   end
 end
--- spec/support/controllers/authenticated_doctor.rb
+++
@@ -1,7 +0,0 @@
-RSpec.configure do |config|
-  config.before(:each, :authenticated_doctor) do
-    pessoa = create(:pessoa, :with_account)
-    create(:medico, pessoa: pessoa)
-    controller.current_pessoa = pessoa
-  end
-end
