70bf1042b6ff6a9fc45d51866cfd23b93cdf64ef
Wagner Bigal <wagnerbigal@gmail.com>
2017-12-05 16:46:59 -0200

    Fixing menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
   delegate :pessoa_signed_in?, to: :controller
   delegate :current_pessoa, to: :controller
+
+  def medico_signed_in?
+    pessoa_signed_in? && current_pessoa.medico.present?
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,4 +8,41 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe '#current_pessoa' do
     it { expect(helper).to delegate_method(:current_pessoa).to(:controller) }
   end
+
+  describe '#medico_signed_in?' do
+    it { expect(helper).to respond_to(:medico_signed_in?) }
+
+    context 'when pessoa is medico' do
+      let(:medico) { create(:medico) }
+
+      before do
+        allow(helper).to receive(:pessoa_signed_in?).and_return(true)
+        allow(helper).to receive(:current_pessoa).and_return(medico.pessoa)
+      end
+
+      it { expect(helper.medico_signed_in?).to be_truthy }
+    end
+
+    context 'when pessoa is not signed in' do
+      let(:medico) { create(:medico) }
+
+      before do
+        allow(helper).to receive(:pessoa_signed_in?).and_return(false)
+        allow(helper).to receive(:current_pessoa).and_return(medico.pessoa)
+      end
+
+      it { expect(helper.medico_signed_in?).to be_falsy }
+    end
+
+    context 'when pessoa is not a medico' do
+      let(:pessoa) { create(:pessoa) }
+
+      before do
+        allow(helper).to receive(:pessoa_signed_in?).and_return(false)
+        allow(helper).to receive(:current_pessoa).and_return(pessoa)
+      end
+
+      it { expect(helper.medico_signed_in?).to be_falsy }
+    end
+  end
 end
